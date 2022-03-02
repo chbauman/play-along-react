7418c87b9905d60296b4c4579c9372948c277713
Christian Baumann <chbauman@student.ethz.ch>
2022-03-02 19:51:50 +0100

    Added lrs

--- play-along/src/scores.ts
+++ play-along/src/scores.ts
@@ -51,23 +51,55 @@ const kannibalenliedMeasureMap: MeasureMap = {
   245: 105,
 };
 
+const linksRadSchlagerMM: MeasureMap = {
+  14: 2,
+  20: 6,
+  48: 22,
+  63: 31,
+  92: 48,
+  99: 52,
+  127: 68,
+  142: 77,
+  171: 94,
+  190: 105,
+  198: 109,
+  208: 115,
+  234: 130,
+};
+
 export const scores: ScoreInfo[] = [
   {
     videoId: "lDQ7hXMLxGc",
     measureMap: matchDataSovietMarch,
     fileName: "soviet_march",
-    name: "Soviet March",
+    name: "Soviet March - Red Alert 3 Theme",
   },
   {
     videoId: "4dIiN57DQOI",
     measureMap: myMotherMatchData,
     fileName: "My_Mother_Told_Me",
-    name: "My Mother Told Me",
+    name: "My Mother Told Me - Saltatio Mortis",
   },
   {
     videoId: "bF4TmtNwM_4",
     measureMap: kannibalenliedMeasureMap,
     fileName: "Kannibalenlied",
-    name: "Kannibalenlied",
+    name: "Kannibalenlied - K.I.Z.",
+  },
+  {
+    videoId: "7ST1YbLwZhs",
+    measureMap: linksRadSchlagerMM,
+    fileName: "Linksradikaler_Schlager",
+    name: "Linksradikaler Schlager - SWISS",
   },
 ];
+
+// Sort alphabetically based on name
+scores.sort((a, b) => {
+  if (a.name < b.name) {
+    return -1;
+  } else if (a.name > b.name) {
+    return 1;
+  }
+  return 0;
+});
