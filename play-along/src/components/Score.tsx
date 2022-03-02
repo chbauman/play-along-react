aa61380e2bcfd7414d4dd463ba4fc48dbb87c75b
Christian Baumann <chbauman@student.ethz.ch>
2022-03-02 21:25:33 +0100

    Better interpolation for smoother transitions and less jumps

--- play-along/src/components/Score.tsx
+++ play-along/src/components/Score.tsx
@@ -1,71 +1,10 @@
 import { useEffect, useRef, useState } from "react";
 import { OpenSheetMusicDisplay } from "opensheetmusicdisplay";
 import { MeasureMap, ScoreInfo } from "../scores";
+import Spline from "typescript-cubic-spline";
 
 const fullW = 40000;
 const leftStaveMarginPx = 1000;
-
-const secsToMeasureNr = (secs: number, matchData: MeasureMap) => {
-  let minIdx: number | null = null;
-  let minDist: number | null = null;
-  let maxIdx: number | null = null;
-  let maxDist: number | null = null;
-  Object.keys(matchData).forEach((el) => {
-    const elInt = parseInt(el);
-    if (elInt >= secs) {
-      const diff = elInt - secs;
-      if (maxDist === null || maxDist > diff) {
-        maxIdx = elInt;
-        maxDist = diff;
-      }
-    }
-    if (elInt <= secs) {
-      const diff = secs - elInt;
-      if (minDist === null || minDist > diff) {
-        minIdx = elInt;
-        minDist = diff;
-      }
-    }
-  });
-
-  if (minIdx === null) {
-    if (maxIdx === null) {
-      throw new Error("Fuck");
-    }
-    return matchData[maxIdx];
-  } else if (maxIdx === null) {
-    return matchData[minIdx];
-  }
-
-  if (minDist === null || maxDist === null) {
-    throw new Error("Shit sucks!");
-  }
-
-  const frac = minDist / (minDist + maxDist);
-  const [minMeas, maxMeas] = [matchData[minIdx], matchData[maxIdx]];
-  const measureNrFloat = minMeas + (maxMeas - minMeas) * frac;
-  return measureNrFloat;
-};
-
-const interpolatedMap = (
-  secs: number,
-  osmd: OpenSheetMusicDisplay | null,
-  measureMap: MeasureMap
-) => {
-  if (!osmd) {
-    return 0;
-  }
-
-  // Interpolate again
-  const measureNr = secsToMeasureNr(secs, measureMap);
-  const staveNr = Math.floor(measureNr);
-  const currMeasure = osmd.GraphicSheet.MeasureList[staveNr - 1];
-  const nextMeasure = osmd.GraphicSheet.MeasureList[staveNr];
-  const xPos = (currMeasure[0] as any).stave.x;
-  const xPosNext = (nextMeasure[0] as any).stave.x;
-  const xPosInterpolated = xPos + (xPosNext - xPos) * (measureNr - staveNr);
-  return Math.max(0, xPosInterpolated - leftStaveMarginPx);
-};
 
 const loadOsmd = async (xmlTxt: string) => {
   const osmd = new OpenSheetMusicDisplay("osmd", {
@@ -79,6 +18,57 @@ const loadOsmd = async (xmlTxt: string) => {
   return osmd;
 };
 
+/** Interpolates from seconds to x position. */
+const interpolate = (
+  osmd: OpenSheetMusicDisplay | null,
+  measureMap: MeasureMap
+) => {
+  if (!osmd) {
+    return null;
+  }
+  const measureList = osmd.GraphicSheet.MeasureList;
+  const measureXList = measureList.map((el) => (el[0] as any)?.stave.x);
+
+  const mmEntries = Object.entries(measureMap);
+  const nEntries = mmEntries.length;
+  const getFromEntry = (idx: number) => {
+    const [secStr, measIdx] = mmEntries[idx];
+    return [parseInt(secStr), measIdx];
+  };
+  let [currSec, currMeasIdx] = getFromEntry(0);
+
+  const secs = [];
+  const xVals = [];
+  for (let k = 0; k < nEntries - 1; ++k) {
+    let [nextSec, nextMeasIdx] = getFromEntry(k + 1);
+    const nextX = measureXList[nextMeasIdx - 1];
+    if (nextX === undefined) {
+      continue;
+    }
+    // Do stuff
+    for (let i = currMeasIdx; i < nextMeasIdx; ++i) {
+      const x = measureXList[i - 1];
+      if (x !== undefined) {
+        const ipSecs =
+          currSec +
+          ((nextSec - currSec) * (i - currMeasIdx)) /
+            (nextMeasIdx - currMeasIdx);
+        secs.push(ipSecs);
+        xVals.push(x);
+      }
+    }
+
+    currSec = nextSec;
+    currMeasIdx = nextMeasIdx;
+  }
+  secs.push(currSec);
+  xVals.push(measureXList[currMeasIdx - 1]);
+
+  // new a Spline object
+  const spline = new Spline(secs, xVals);
+  return (val: number) => Math.max(0, spline.at(val) - leftStaveMarginPx);
+};
+
 export const Score = (props: {
   xmlTxt: string;
   scoreInfo: ScoreInfo;
@@ -87,13 +77,19 @@ export const Score = (props: {
   const osmdRef = useRef<any>();
 
   const [currXPos, setCurrXPos] = useState(0);
+  const [osmdSet, setOsmdSet] = useState(false);
 
   useEffect(() => {
     const loadLocal = async () => {
       const osmd = await loadOsmd(props.xmlTxt);
       osmdRef.current = osmd;
+      setOsmdSet(true);
     };
     loadLocal();
+    return () => {
+      setOsmdSet(false);
+      osmdRef.current = null;
+    };
   }, [props.xmlTxt]);
 
   const { innerWidth: width } = window;
@@ -101,20 +97,19 @@ export const Score = (props: {
 
   const { getTime, scoreInfo } = props;
   useEffect(() => {
+    const ipObj = interpolate(osmdRef.current, scoreInfo.measureMap);
     const interval = setInterval(async () => {
-      const elapsedSec = await getTime();
-      const xPos = interpolatedMap(
-        elapsedSec,
-        osmdRef.current,
-        scoreInfo.measureMap
-      );
-      setCurrXPos(xPos);
+      if (ipObj) {
+        const elapsedSec = await getTime();
+        const xPos = ipObj(elapsedSec);
+        setCurrXPos(xPos);
+      }
     }, 20); // ms refresh.
 
     return () => {
       clearInterval(interval);
     };
-  }, [getTime, scoreInfo]);
+  }, [getTime, scoreInfo, osmdSet]);
 
   return (
     <>
