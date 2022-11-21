71c9696d584fcebdd3875cec478deaa2283183f5
Christian Baumann <chbauman@student.ethz.ch>
2022-11-21 13:51:31 +0100

    Update reduce.py

--- reduce.py
+++ reduce.py
@@ -8,10 +8,23 @@ REDUCED_SCORES_PATH = PUBLIC_PATH / "reduced"
 
 REDUCED_SCORES_PATH = PUBLIC_PATH / "scores"
 
-REMOVE_ELEMENTS = ["scaling", "page-layout"]
+REMOVE_ELEMENTS = [
+    "defaults",
+    "credit",
+    "print",
+    "work",
+    "midi-device",
+    "midi-instrument",
+    "voice",
+    "type",
+    # "beam",
+    "stem",
+]
+REMOVE_ATRS = ["default-x", "default-y", "width"]
 XML_DEC = """<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE score-partwise PUBLIC "-//Recordare//DTD MusicXML 3.1 Partwise//EN" "http://www.musicxml.org/dtds/partwise.dtd">
 """
+
 
 def reduce_file(path: Path):
     out_path = REDUCED_SCORES_PATH / path.name
@@ -19,10 +32,17 @@ def reduce_file(path: Path):
     tree = ET.parse(path)
     root = tree.getroot()
     for el_name in REMOVE_ELEMENTS:
-        for el in root.findall(el_name):
-            root.remove(el)
+        for el in root.findall(f".//*[{el_name}]"):
+            for sub_el in el.findall(el_name):
+                el.remove(sub_el)
+
+    for attr_name in REMOVE_ATRS:
+        for el in root.findall(f".//*[@{attr_name}]"):
+            el.attrib.pop(attr_name)
+            pass
+
     print(path)
-    with open(out_path, 'wb') as f:
+    with open(out_path, "wb") as f:
         f.write(XML_DEC.encode("UTF-8"))
         tree.write(f, encoding="UTF-8")
 
