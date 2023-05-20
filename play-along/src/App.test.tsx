c37e6f1a9f6b0cc010ded1366ad84f63a57e5423
Christian Baumann <chbauman@student.ethz.ch>
2023-05-20 20:10:10 +0200

    Update App.test.tsx

--- play-along/src/App.test.tsx
+++ play-along/src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders app", () => {
   render(<App />);
   const linkElement = screen.getByText("Youtube Play Along");
   expect(linkElement).toBeInTheDocument();
